dec0b9e0d5e8bfbb3177cb78d9762675b50e9892
katiheikkonen <kati.m.heikkonen@gmail.com>
2020-07-06 10:53:35 +0300

    Added person class

--- git_pro.py
+++ git_pro.py
@@ -1,2 +1,10 @@
 def hello():
     return('Hello dev!')
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        print(f"Name of the person is {self.name} and age {self.age})
